e733a47fdb5b4ca44416de6d85bf4a2e9b8f0f52
szkrecsi <ecsenszki.judit@gmail.com>
2017-06-11 23:33:36 +0200

    Add files via upload

---
+++ TimerForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TimerEvents
+{
+    partial class TimerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 23);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(311, 51);
+            this.progressBar.TabIndex = 0;
+            // 
+            // TimerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 86);
+            this.Controls.Add(this.progressBar);
+            this.Name = "TimerForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Timer_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ TimerForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class TimerForm : Form
+    {
+        //Timer objects can be used to throw events after a specified number of milliseconds
+        System.Windows.Forms.Timer timer;
+
+        public TimerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Shown(object sender, EventArgs e)
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value += 10;
+            if (progressBar.Value >= 100) timer.Stop();
+        }
+    }
+}
